d81733c89faf2c16db6edfd4bd04aab5af3e3c89
Yasser Algburi <yasseralgburi@gmail.com>
2025-11-24 17:57:33 -0500

    handling game control logic

--- src/components/GameControls.jsx
+++ src/components/GameControls.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 
-export default function gameControls({
+export default function GameControls({
   isPlayerTurn,
   onPass,
   canPass,
   gamePhase,
   turnMessage,
 }) {
-  function handlePassClick() {}
+  function handlePassClick() {
+    if (!isButtonDisabled()) {
+      onPass();
+    }
+  }
 
-  function isButtonDisabled() {}
+  function isButtonDisabled() {
+    return !isPlayerTurn || !canPass;
+  }
 
-  function getButtonText() {}
-
-  return <div></div>;
+  function getButtonText() {
+    if (isButtonDisabled()) {
+      if (!isPlayerTurn) {
+        return "Opponent's Turn";
+      }
+      if (!canPass) {
+        return "Cannot Pass Yet";
+      }
+    }
+    return "PASS";
+  }
 }
