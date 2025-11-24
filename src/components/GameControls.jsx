6892f4aae9d99993eac8ad28a4391f83f961ef57
Yasser Algburi <yasseralgburi@gmail.com>
2025-11-24 17:16:28 -0500

    file setup

--- src/components/GameControls.jsx
+++ src/components/GameControls.jsx
@@ -1,1 +1,17 @@
-// test
+import React from "react";
+
+export default function gameControls({
+  isPlayerTurn,
+  onPass,
+  canPass,
+  gamePhase,
+  turnMessage,
+}) {
+  function handlePassClick() {}
+
+  function isButtonDisabled() {}
+
+  function getButtonText() {}
+
+  return <div></div>;
+}
