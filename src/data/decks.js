8289d57a27a8baded9109b589b4b52d837937e7d
Yasser Algburi <yasseralgburi@gmail.com>
2025-11-25 16:05:16 -0500

    working on deck logic

--- src/data/decks.js
+++ src/data/decks.js
@@ -1,6 +1,26 @@
-function createDecks() {}
+function createDecks() {
+  let cards = [];
 
-function shuffleDeck(deck) {}
+  cards.forEach((card) => {
+    if (card.hero) {
+      cards.push(card.hero);
+    }
+    if (!card.hero) {
+      cards.push(card.count);
+    }
+    cards.randomUUID();
+  });
+  return cards;
+}
+
+function shuffleDeck(deck) {
+  let deckClone = [...deck];
+
+  for (let i = deckClone.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [deckClone[i], deckClone[j]] = [deckClone[j], deckClone[i]];
+  }
+}
 
 function drawStartingHand(deck) {}
 
