47bf903b83a998360b790834955888b2692a658e
Yasser Algburi <yasseralgburi@gmail.com>
2025-11-25 17:28:41 -0500

    fixed some bugs

--- src/data/decks.js
+++ src/data/decks.js
@@ -1,28 +1,37 @@
-function createDecks() {
-  let cards = [];
+export function createDeck(cardDefinitions) {
+  const deck = [];
 
-  cards.forEach((card) => {
-    if (card.hero) {
-      cards.push(card.hero);
+  cardDefinitions.forEach((card) => {
+    const copiesToAdd = card.hero ? 1 : card.count;
+
+    for (let i = 0; i < copiesToAdd; i++) {
+      const cardInstance = {
+        ...card,
+        instanceId: `${card.id}_${i}_${Date.now()}_${Math.random()}`,
+      };
+      deck.push(cardInstance);
     }
-    if (!card.hero) {
-      cards.push(card.count);
-    }
-    cards.randomUUID();
   });
-  return cards;
+
+  return deck;
 }
 
-function shuffleDeck(deck) {
+export function shuffleDeck(deck) {
   let deckClone = [...deck];
 
   for (let i = deckClone.length - 1; i >= 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [deckClone[i], deckClone[j]] = [deckClone[j], deckClone[i]];
   }
+  return deckClone;
 }
 
-function drawStartingHand(deck) {}
+function drawStartingHand(deck) {
+  const hand = deck.slice(0, 10);
+  const remainingDeck = deck.slice(10);
+
+  return { hand, remainingDeck };
+}
 
 function drawCards(deck, count) {}
 
